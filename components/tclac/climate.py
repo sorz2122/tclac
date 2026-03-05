3625ea647b5e2fef80caea966d29d9fc815a0173
sorz2122 <81297488+sorz2122@users.noreply.github.com>
2026-03-05 20:38:58 +0100

    Fix: correct indentation in to_code function for ESPHome 2026.x

--- components/tclac/climate.py
+++ components/tclac/climate.py
@@ -311,9 +311,9 @@ async def tclac_set_horizontal_swing_direction_to_code(config, action_id, templa
 # Добавление конфигурации в код
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-        await cg.register_component(var, config)
-        await uart.register_uart_device(var, config)
-        await climate.register_climate(var, config)
+    await cg.register_component(var, config)
+        t uart.register_uart_device(var, config)
+    await climate.register_climate(var, config)
 
     if CONF_BEEPER in config:
         cg.add(var.set_beeper_state(config[CONF_BEEPER]))
